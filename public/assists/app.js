bba94522f5a3312540ec71e6341a10b36851d407
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-06-24 16:36:41 -0700

    added chartjs

--- public/assists/app.js
+++ public/assists/app.js
@@ -16,7 +16,79 @@ window.onscroll = function () {
     $(this).removeClass("hover");
   }
 );
-// scrolling
+// scrolling animations
 AOS.init({
-  duration: 1000,
+  duration: 900,
 })
+
+// pie
+var options = {
+  animation: {
+    animateScale: false,
+    animateRotate: true
+  },
+
+  rotation: 1 * Math.PI,
+  circumference: 1 * Math.PI,
+
+  tooltips: {
+    backgroundColor: "#FFF",
+    titleFontSize: 16,
+    titleFontColor: "#0066ff",
+    bodyFontColor: "#000",
+    bodyFontSize: 14,
+    displayColors: false
+  },
+
+  responsive:true,
+  maintainAspectRatio: false,
+
+
+};
+
+
+var codeSkills = {
+  labels: ["Git", "HTML-CSS", "Node.js", "Javascript"],
+  datasets: [
+    {
+      backgroundColor: [
+        "#488f31",
+        "#a5a73f",
+        "#f4bd6a",
+        "#eb8050"
+      ],
+      data: [10, 20, 30, 40]
+    }
+  ]
+};
+
+var peopleSkills = {
+  labels: ["Adaptability", "Leadership", "Critical Thinking", "TeamWork"],
+  datasets: [
+    {
+      backgroundColor: [
+        "#003f5c",
+        "#58508d",
+        "#bc5090",
+        "#ff6361"
+      ],
+      data: [10, 20, 30, 40]
+    }
+  ]
+};
+
+
+
+var code = document.getElementById("myChart").getContext("2d");
+var chart = new Chart(code, {
+  type: "doughnut",
+  data: codeSkills,
+  options: options
+});
+
+var skills = document.getElementById("skills").getContext("2d");
+var chart = new Chart(skills, {
+  type: "doughnut",
+  data: peopleSkills,
+  options: options
+});
